460845b67fedf9559aae84e54406fa27ee0262c7
Siyuan <shensiyuan25@gmail.com>
2020-04-21 17:37:43 -0400

    filter and menu page

--- search.php
+++ search.php
@@ -5,6 +5,8 @@ require("food_db.php");
 $msg = '';
 
 $all_restaurants = getAllRestaurants();
+$all_cuisines = getAllCuisines();
+
 ?>
 
 
@@ -21,101 +23,117 @@ $all_restaurants = getAllRestaurants();
 
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <title>Food App</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <link rel="stylesheet" href="menu_template/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="menu_template/css/animate.css">
 
-  <title>Food</title>
+    <link rel="stylesheet" href="menu_template/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="menu_template/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="menu_template/css/magnific-popup.css">
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="menu_template/css/aos.css">
 
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+    <link rel="stylesheet" href="menu_template/css/ionicons.min.css">
 
-</head>
+    <link rel="stylesheet" href="menu_template/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="menu_template/css/jquery.timepicker.css">
 
-<body>
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <link rel="stylesheet" href="menu_template/css/flaticon.css">
+    <link rel="stylesheet" href="menu_template/css/icomoon.css">
+    <link rel="stylesheet" href="menu_template/css/style.css">
+  </head>
+  <body class="goto-here">
+		<div class="py-1 bg-primary">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
+	    		<div class="col-lg-12 d-block">
+		    		<div class="row d-flex">
+		    			<div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <span class="text">+ 1235 2355 98</span>
+					    </div>
+					    <div class="col-md pr-4 d-flex topper align-items-center">
+					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+						    <span class="text">youremail@email.com</span>
+					    </div>
+					    <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
+						    <span class="text">3-5 Business days delivery &amp; Free Returns</span>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="search.php">Food App</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item active dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="shop.html">Shop</a>
+              	<a class="dropdown-item" href="wishlist.html">Wishlist</a>
+                <a class="dropdown-item" href="product-single.html">Single Product</a>
+                <a class="dropdown-item" href="cart.html">Cart</a>
+                <a class="dropdown-item" href="checkout.html">Checkout</a>
+              </div>
+            </li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+
     <div class="container">
-      <a class="navbar-brand" href="#">Food App</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Search
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Order</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Account</a>
-        </ul>
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+        	<!-- <p class="breadcrumbs"><span class="mr-2"><h href="index.html">Home</h></span> <span>Products</span></p> -->
+          <h class="mb-0 bread" style="color:black; font-size:30px">Restaurants</h>
+        </div>
       </div>
     </div>
-  </nav>
 
 
+    <section class="ftco-section">
+
+    	<div class="container">
+        <!--
+    		<div class="row justify-content-center">
+    			<div class="col-md-10 mb-5 text-center">
+    				<ul class="product-category">
+    					<li><a href="#" class="active">All</a></li>
+    					<li><a href="#">Vegetables</a></li>
+    					<li><a href="#">Fruits</a></li>
+    					<li><a href="#">Juice</a></li>
+    					<li><a href="#">Dried</a></li>
+    				</ul>
+    			</div>
+    		</div>
+      -->
 
 
-  <!-- Page Content -->
-  <div class="container">
-
-    <div class="row">
-
-      <div class="col-lg-3">
-
-        <h1 class="my-4">Food</h1>
-        <div class="list-group">
-          <a href="#" class="list-group-item">Category 1</a>
-          <a href="#" class="list-group-item">Category 2</a>
-          <a href="#" class="list-group-item">Category 3</a>
-        </div>
-
-      </div>
-      <!-- /.col-lg-3 -->
-
-      <div class="col-lg-9">
-
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-
-
-        <!-- Search Bar -->
+        <!-- Search bar  -->
         <div class="search_bar">
           <input class = "form-control mb-4" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for restaurants" title="Search for restaurants">
           <script>
@@ -136,17 +154,67 @@ $all_restaurants = getAllRestaurants();
               }
           }
           </script>
+
+
+        </div>
+        <!-- end search bar -->
+
+        <!-- Filters -->
+
+        <div class="row">
+          <div class="col-lg-6 product-details pl-md-5 ftco-animate">
+                <div class="form-group d-flex">
+                  <form action="search.php" method="post">
+                    <div class="select-wrap">
+                      <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                      <select name="" id="cuisine_filter" class="form-control">
+                        <option value="None" >Cuisine</option>
+                        <?php foreach ($all_cuisines as $cuisine): ?>
+                          <option value="<?php echo $cuisine["cuisine"];?>"> <?php echo $cuisine["cuisine"];?></option>
+                        <?php endforeach; ?>
+                      </select>
+                    </div>
+                    <button type="button" onclick="cuisineFilter()">Apply Filter</button>
+                  </form>
+                </div>
+            </div>
+
         </div>
 
+        <script>
+            function cuisineFilter(){
+              var filter, row, res_cards, a, i, txtValue;
+              var e = document.getElementById("cuisine_filter");
+              var result = e.options[e.selectedIndex].text;
+              //document.getElementById("result").innerHTML = result;
+              filter = result.toUpperCase();
+              row = document.getElementById("row");
+              res_cards = row.getElementsByClassName("col-lg-4 col-md-6 mb-4")
+              for (i = 0; i < res_cards.length; i++) {
+                  if (result == 'Cuisine'){
+                    res_cards[i].style.display = "";
+                    continue
+                  }
+                  a = res_cards[i].getElementsByTagName("h5")[0];
+                  txtValue = a.innerText;
+                  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                      res_cards[i].style.display = "";
+                  } else {
+                      res_cards[i].style.display = "none";
+                  }
+                }
+          }
+        </script>
 
-        <!-- Row -->
+        <!-- end Filters -->
 
-        <body>
+
+
         <div id = "row" class="row">
           <?php foreach ($all_restaurants as $restaurant): ?>
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <a href="#"><img class="card-img-top"  alt=""></a>
               <!-- <a href="#"><img class="card-img-top" src="blue.jpg" alt="" style="width:700;height:400;"></a> -->
               <div class="card-body">
                 <h4 class="card-title">
@@ -179,36 +247,166 @@ $all_restaurants = getAllRestaurants();
           </div>
           <?php endforeach; ?>
         </div>
-
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <style>
         .checked {
           color: orange;
         }
         </style>
-        <!-- /.row -->
 
+
+
+    <div class="row">
+      <div class="col-lg-6 product-details pl-md-5 ftco-animate">
+            <div class="form-group d-flex">
+              <div class="select-wrap">
+                <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                <select name="" id="" class="form-control">
+                  <option value="">Small</option>
+                  <option value="">Medium</option>
+                  <option value="">Large</option>
+                  <option value="">Extra Large</option>
+                </select>
+              </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    		<div class="row mt-5">
+          <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    	</div>
+    </section>
+
+		<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
+      <div class="container py-4">
+        <div class="row d-flex justify-content-center py-5">
+          <div class="col-md-6">
+          	<h2 style="font-size: 22px;" class="mb-0">Subcribe to our Newsletter</h2>
+          	<span>Get e-mail updates about our latest shops and special offers</span>
+          </div>
+          <div class="col-md-6 d-flex align-items-center">
+            <form action="#" class="subscribe-form">
+              <div class="form-group d-flex">
+                <input type="text" class="form-control" placeholder="Enter email address">
+                <input type="submit" value="Subscribe" class="submit px-3">
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <!-- /.col-lg-9 -->
+    </section>
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+      	<div class="row">
+      		<div class="mouse">
+						<a href="#" class="mouse-icon">
+							<div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
+						</a>
+					</div>
+      	</div>
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Food App</h2>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Menu</h2>
+              <ul class="list-unstyled">
+                <li><a href="#" class="py-2 d-block">Shop</a></li>
+                <li><a href="#" class="py-2 d-block">About</a></li>
+                <li><a href="#" class="py-2 d-block">Journal</a></li>
+                <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Help</h2>
+              <div class="d-flex">
+	              <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+	                <li><a href="#" class="py-2 d-block">Shipping Information</a></li>
+	                <li><a href="#" class="py-2 d-block">Returns &amp; Exchange</a></li>
+	                <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
+	                <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
+	              </ul>
+	              <ul class="list-unstyled">
+	                <li><a href="#" class="py-2 d-block">FAQs</a></li>
+	                <li><a href="#" class="py-2 d-block">Contact</a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
 
-    </div>
-    <!-- /.row -->
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						</p>
+          </div>
+        </div>
+      </div>
+    </footer>
 
-  </div>
-  <!-- /.container -->
 
-  <!-- Footer -->
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-</body>
 
+  <script src="menu_template/js/jquery.min.js"></script>
+  <script src="menu_template/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="menu_template/js/popper.min.js"></script>
+  <script src="menu_template/js/bootstrap.min.js"></script>
+  <script src="menu_template/js/jquery.easing.1.3.js"></script>
+  <script src="menu_template/js/jquery.waypoints.min.js"></script>
+  <script src="menu_template/js/jquery.stellar.min.js"></script>
+  <script src="menu_template/js/owl.carousel.min.js"></script>
+  <script src="menu_template/js/jquery.magnific-popup.min.js"></script>
+  <script src="menu_template/js/aos.js"></script>
+  <script src="menu_template/js/jquery.animateNumber.min.js"></script>
+  <script src="menu_template/js/bootstrap-datepicker.js"></script>
+  <script src="menu_template/js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="menu_template/js/google-map.js"></script>
+  <script src="menu_template/js/main.js"></script>
+
+  </body>
 </html>
